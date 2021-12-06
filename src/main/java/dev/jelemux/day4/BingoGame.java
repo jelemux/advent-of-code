28fb79297d44aacbcb84be528d6771f4c100bf38
jelemux <jeremias.weber@protonmail.com>
2021-12-06 11:36:27 +0100

    day 4 puzzle 2

--- src/main/java/dev/jelemux/day4/BingoGame.java
+++ src/main/java/dev/jelemux/day4/BingoGame.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -27,39 +26,32 @@ public class BingoGame {
     }
 
     @NonNull
-    private Optional<WinningState> checkNumber(final int number) {
+    private List<WinningState> checkNumber(final int number) {
+        final var winningOrder = new ArrayList<WinningState>();
         final var fields = this.numberMapping.get(number);
         for (final var field: fields) {
             final var bingo = field.check();
             if (bingo) {
                 final var winningState = new WinningState(field.getRow().getBoard(), number);
-                return Optional.of(winningState);
+                winningOrder.add(winningState);
             }
         }
-        return Optional.empty();
+        return winningOrder;
     }
 
     @NonNull
     private List<WinningState> checkNumbers(final int[] numbers) {
         final var winningOrder = new ArrayList<WinningState>();
         for (final var number: numbers) {
-            final var winner = this.checkNumber(number);
-            if (winner.isPresent()) {
-                winningOrder.add(winner.get());
-            }
+            final var winners = this.checkNumber(number);
+            winningOrder.addAll(winners);
         }
         return winningOrder;
     }
 
     @NonNull
     public List<WinningState> calculateWinningOrder() {
-        final var winningOrder = checkNumbers(drawnNumbers);
-
-        for (final var ws : winningOrder) {
-            System.out.println("Board: " + ws.getBoard().getId() + "; Score: " + ws.getScore());
-        }
-
-        return winningOrder;
+        return checkNumbers(drawnNumbers);
     }
     
 }
